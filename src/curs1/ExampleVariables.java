d8fe9dd7e450d2fdd4c12c40c82c930fc7599b89
Claudia Timiș <claudia.timis95@gmail.com>
2022-02-24 18:46:54 +0200

    Initiall Commit

---
+++ src/curs1/ExampleVariables.java
@@ -0,0 +1,18 @@
+package curs1;
+
+public class ExampleVariables {
+
+    String prenume  = "Oana";
+
+    public static void main (String[] args) {
+
+        ExampleVariables numeObiect = new ExampleVariables ();
+        System.out.println(numeObiect.prenume);
+    }
+
+    public void printNume() {
+        String nume = "Ion";
+        System.out.println(nume);
+    }
+
+}
